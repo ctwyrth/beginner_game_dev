8df3164ca592f9d17cca35c68fdc52335013615b
Thomas W <96929397+ctwyrth@users.noreply.github.com>
2023-01-26 10:33:45 -0800

    2nd Commit, more sprite work

--- sprite_animation/script.js
+++ sprite_animation/script.js
@@ -6,23 +6,78 @@ const CANVAS_WIDTH = canvas.width = 600;
 const CANVAS_HEIGHT = canvas.height = 600;
 const spriteWidth = 575;
 const spriteHeight = 523;
-let frameX = 0;
-let frameY = 0;
 let gameFrame = 0;
 const staggerFrames = 5;
+const spriteAnimations = [];
+const animationStates = [
+   {
+      name: 'idle',
+      frames: 7,
+   },
+   {
+      name: 'jump',
+      frames: 7,
+   },
+   {
+      name: 'fall',
+      frames: 7,
+   },
+   {
+      name: 'run',
+      frames: 9,
+   },
+   {
+      name: 'dizzy',
+      frames: 11,
+   },
+   {
+      name: 'sit',
+      frames: 5,
+   },
+   {
+      name: 'roll',
+      frames: 7,
+   },
+   {
+      name: 'bite',
+      frames: 7,
+   },
+   {
+      name: 'ko',
+      frames: 12,
+   },
+   {
+      name: 'getHit',
+      frames: 4,
+   },
+];
+animationStates.forEach((state, index) => {
+   let frames = {
+      loc: [],
+   }
+   for (let j = 0; j < state.frames; j++) {
+      let positionX = j * spriteWidth;
+      let positionY = index * spriteHeight;
+      frames.loc.push({ x: positionX, y: positionY });
+   }
+   spriteAnimations[state.name] = frames;
+});
+
 
 const playerImage = new Image();
 playerImage.src = 'shadow_dog.png';
 
 function animate() {
    ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+   let position = Math.floor(gameFrame/staggerFrames) % spriteAnimations['dizzy'].loc.length;
+   let frameX = spriteWidth * position;
+   let frameY = spriteAnimations['dizzy'].loc[position].y;
    // ctx.fillRect(50, 50, 100, 100);
    // ctx.drawImage(image, sx, sy, sw, sh, dx, dy, dw, dh);
-   ctx.drawImage(playerImage, frameX * spriteWidth, frameY * spriteHeight, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
-   if (gameFrame % staggerFrames === 0) {
-      if (frameX < 6) frameX++;
-      else frameX = 0; 
-   }
+   ctx.drawImage(playerImage, frameX, frameY, spriteWidth, spriteHeight, 0, 0, spriteWidth, spriteHeight);
+
+   gameFrame++;
    requestAnimationFrame(animate);
 };
 
